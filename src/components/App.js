ecd4e226b211e74479e0815e0f2ff000abed6a22
ahhzaky <siahmadzaky@gmail.com>
2020-09-23 22:19:50 +0700

    Setup prevOpen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 
 function App() {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        <html/>
+        `
+      );
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
   return (
     <>
       <div className="pane top-pane">
@@ -29,6 +47,7 @@ function App() {
       </div>
       <div className="pane">
         <iframe
+          srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
